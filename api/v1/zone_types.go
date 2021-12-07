77c3fa61d367cb5207533369508bcf7e6760c853
Chris <github.account@chrigel.net>
2021-12-07 13:00:31 +0100

    Define Zone API spec

--- api/v1/zone_types.go
+++ api/v1/zone_types.go
@@ -1,6 +1,9 @@
 package v1
 
 import (
+	"fmt"
+	"net/url"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -12,12 +15,44 @@ type Zone struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	// Data holds the cluster specific metadata.
 	Data ZoneData `json:"data,omitempty"`
 }
 
 // ZoneData holds all the Zone specific properties
 type ZoneData struct {
-	// TODO: Fill out spec
+	// DisplayName is a human-friendly name for the Zone.
+	DisplayName string `json:"displayName,omitempty"`
+	// Features holds a key-value dict with keys being a feature name and values being a property of that feature.
+	// Some features may hold a version string as property.
+	Features Features `json:"features,omitempty"`
+	// URLs holds a key-value dict with keys being a name of the URL and the values publicly accessible links.
+	URLs URLMap `json:"urls,omitempty"`
+	// CNAME is the DNS record where custom application DNS hostnames shall be pointing to when exposing an application.
+	CNAME string `json:"cname,omitempty"`
+	// DefaultAppDomain is the base DNS record where OpenShift Routes without specific hostnames are exposed.
+	DefaultAppDomain string `json:"defaultAppDomain,omitempty"`
+	// GatewayIPs holds the outgoing IP addresses of the cluster.
+	GatewayIPs []string `json:"gatewayIPs,omitempty"`
+	// CloudProvider identifies the infrastructure provider which the Zone is running on.
+	CloudProvider CloudProvider `json:"cloudProvider,omitempty"`
+}
+
+// Features is a key-value dict with keys being a feature name and values being a property of that feature.
+type Features map[string]string
+
+// URLMap is a key-value dict with keys being a name of the URL and the values publicly accessible links.
+type URLMap map[string]string
+
+// CloudProvider identifies an infrastructure provider.
+type CloudProvider struct {
+	// Name identifies the cloud provider.
+	Name string `json:"name,omitempty"`
+	// Zones is cloud-provider-specific zone aliases within a Region.
+	// If multiple entries are present, the cluster may be spanning multiple zones.
+	Zones []string `json:"zones,omitempty"`
+	// Region is the geographic location of the Zone.
+	Region string `json:"region,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -28,6 +63,17 @@ type ZoneList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 
 	Items []Zone `json:"items"`
+}
+
+// GetURL invokes url.Parse for the raw string from given key, if found.
+func (in URLMap) GetURL(key string) (*url.URL, error) {
+	if in == nil {
+		return nil, fmt.Errorf("map is nil")
+	}
+	if raw, found := in[key]; found {
+		return url.Parse(raw)
+	}
+	return nil, fmt.Errorf("key not found: %s", key)
 }
 
 func init() {
