a7052978e091a35eeadd3c0fec751a104c45955a
Fabian Fischer <glrf@users.noreply.github.com>
2022-01-18 15:51:57 +0100

    Fix return value when deleting Organizations
    Return a valid organization object to prevent missleading errors when
    deleting organizations

--- apiserver/organization/delete_test.go
+++ apiserver/organization/delete_test.go
@@ -13,6 +13,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
@@ -33,12 +34,15 @@ func TestOrganizationStorage_Delete(t *testing.T) {
 		err          error
 	}{
 		"GivenDeleteOrg_ThenSuccess": {
-			name: "foo",
+			name:               "foo",
+			namespaceDeleteErr: nil,
+			namespace:          fooNs,
 			authDecision: authResponse{
 				decision: authorizer.DecisionAllow,
 			},
-			organization: fooOrg,
-			namespace:    fooNs,
+			organization: &orgv1.Organization{
+				ObjectMeta: metav1.ObjectMeta{Name: "foo"},
+			},
 		},
 		"GivenDeleteNonOrg_ThenFail": {
 			name:      "default",
