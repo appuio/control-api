5a8de51b3c69b19a7a2ef6254a020f8940b55db6
Sebastian Widmer <sebastian.widmer@vshn.net>
2023-02-22 16:54:05 +0100

    Expose org/be link

--- controller.go
+++ controller.go
@@ -15,6 +15,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	"github.com/spf13/cobra"
@@ -106,6 +107,11 @@ func setupManager(usernamePrefix, rolePrefix string, memberRoles []string, beRef
 	if err != nil {
 		return nil, err
 	}
+
+	metrics.Registry.MustRegister(
+		&controllers.OrgBillingRefLinkMetric{
+			Client: mgr.GetClient(),
+		})
 
 	ur := &controllers.UserReconciler{
 		Client:   mgr.GetClient(),
