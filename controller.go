d1ce901c86ab55453dc6b9d180b06efc1d127b9e
Christian Häusler <794584+corvus-ch@users.noreply.github.com>
2023-04-17 14:58:40 +0200

    Apply suggestions from code review
    Co-authored-by: Simon Gerber <simon.gerber@vshn.ch>

--- controller.go
+++ controller.go
@@ -37,7 +37,7 @@ const (
 
 You've been invited to join APPUiO Cloud. Please follow https://portal.dev/invitations/{{.Invitation.ObjectMeta.Name}}?token={{.Invitation.Status.Token}} to accept.
 
-Please check out https://docs.appuio.cloud/user/tutorials/getting-started.html on how to get sarted with APPUiO Cloud and https://appuio.cloud to learn more about this service in general. 
+Please check out https://docs.appuio.cloud/user/tutorials/getting-started.html on how to get started with APPUiO Cloud and https://appuio.cloud to learn more about this service in general. 
 
 Best regards,
 APPUiO Cloud Team`
